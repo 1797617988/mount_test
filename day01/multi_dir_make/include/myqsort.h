a0709d29c666ddeb0a7efb158d87eb3a24cd0124
LiXinyuan <lixinyuan@sensing-world.com>
2025-10-29 21:35:06 +0800

    Remove all files

--- day01/multi_dir_make/include/myqsort.h
+++
@@ -1,8 +0,0 @@
-
-
-#ifndef __H_MYQSORT__
-#define __H_MYQSORT__
-
-int myqsort(int arr[], int len);
-
-#endif
--- day01/test.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-void Hello(const char * arg)
-{
-	printf("Hello %s\n", arg);
-}
